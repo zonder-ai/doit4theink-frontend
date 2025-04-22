a518c66ee550c2805c4fdd872945bb0d757e8989
zonder-ai <guille@zonder.ai>
2025-04-22 16:24:18 +0200

    Update next.config.js for standalone output

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   images: {
     domains: ['fmkzfzlujrmwipkoubmm.supabase.co'],
+    unoptimized: process.env.NODE_ENV === 'production',
   },
+  output: 'standalone',
+  // Add any other configurations here
 }
 
 module.exports = nextConfig
